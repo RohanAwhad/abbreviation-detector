ab800fdded308d0ced025663d07c853d0901b916
Rohan Awhad <rohanavad007@gmail.com>
2022-04-26 18:25:18 +0530

    resolved bugs

--- detector.py
+++ detector.py
@@ -59,10 +59,6 @@ class AbbreviationDetector:
         self.get_long_short_pairs(sentences, synced_predictions, doc)
         self._tag_short_forms(sentences, synced_predictions, doc)
 
-        for sent, labels in zip(text_sents, synced_predictions):
-            for word, token in zip(sent, labels):
-                print(f'{word}: {token}')
-
         return doc
 
     def _get_sents(self, doc: Doc):
@@ -147,7 +143,7 @@ class AbbreviationDetector:
                 if b_found and label == "O":
                     short_form = doc[_tmp[0]: _tmp[-1]+1]
                     short_form._.is_short_form = True
-                    short_form._.long_form = doc._.short2long[short_form.text]
+                    short_form._.long_form = doc._.short2long.get(short_form.text, None)
                     short_form_list.append(short_form)
 
                     b_found = False
@@ -278,7 +274,7 @@ if __name__ == '__main__':
     nlp.add_pipe('abbreviation_detector')
 
     text = "The 2002-3 pandemic caused by severe acute respiratory syndrome coronavirus (SARS-CoV) was one of the most significant public health events in recent history(1). An ongoing outbreak of Middle East respiratory syndrome coronavirus (MERS-CoV)(2) suggests that this group of viruses remains a major threat and that their distribution is wider than previously recognized. Although bats have been suggested as the natural reservoirs of both viruses(3-5), attempts to isolate the progenitor virus of SARS-CoV from bats have been unsuccessful. Diverse SARS-like coronaviruses (SL-CoVs) have now been reported from bats in China, Europe and Africa(5-8), but none are considered a direct progenitor of SARS-CoV because of their phylogenetic disparity from this virus and the inability of their spike proteins (S) to use the SARS-CoV cellular receptor molecule, the human angiotensin converting enzyme II (ACE2)(9,10). Here, we report whole genome sequences of two novel bat CoVs from Chinese horseshoe bats (Family: Rhinolophidae) in Yunnan, China; RsSHC014 and Rs3367. These viruses are far more closely related to SARS-CoV than any previously identified bat CoVs, particularly in the receptor binding domain (RDB) of the S protein. Most importantly, we report the first recorded isolation of a live SL-CoV (bat SL-CoV-WIV1) from bat fecal samples in Vero E6 cells, which has typical coronavirus morphology, 99.9% sequence identity to Rs3367 and uses the ACE2s from human, civet and Chinese horseshoe bat for cell entry. Preliminary in vitro testing indicates that WIV1 also has a broad species tropism. Our results provide the strongest evidence to date that Chinese horseshoe bats are natural reservoirs of SARS-CoV, and that intermediate hosts may not be necessary for direct human infection by some bat SL-CoVs. They also highlight the importance of pathogen discovery programs targeting high-risk wildlife groups in emerging disease hotspots as a strategy for pandemic preparedness."
-    text = 'Although bats have been suggested as the natural reservoirs of both viruses(3-5), attempts to isolate the progenitor virus of SARS-CoV from bats have been unsuccessful. Diverse SARS-like coronaviruses (SL-CoVs) have now been reported from bats in China, Europe and Africa(5-8), but none are considered a direct progenitor of SARS-CoV because of their phylogenetic disparity from this virus and the inability of their spike proteins (S) to use the SARS-CoV cellular receptor molecule, the human angiotensin converting enzyme II (ACE2) (9,10)'
+    #text = 'Although bats have been suggested as the natural reservoirs of both viruses(3-5), attempts to isolate the progenitor virus of SARS-CoV from bats have been unsuccessful. Diverse SARS-like coronaviruses (SL-CoVs) have now been reported from bats in China, Europe and Africa(5-8), but none are considered a direct progenitor of SARS-CoV because of their phylogenetic disparity from this virus and the inability of their spike proteins (S) to use the SARS-CoV cellular receptor molecule, the human angiotensin converting enzyme II (ACE2) (9,10)'
     doc = nlp(text)
     print(doc._.abbreviations)
     print(doc._.short2long)
@@ -286,4 +282,4 @@ if __name__ == '__main__':
         start = short_form['start']
         end = short_form['end']
         long_form = doc[start:end]._.long_form
-        print(short_form['text'], ':', long_form)
+        print(short_form['short_form'], ':', long_form)
