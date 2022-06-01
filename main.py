08a5e098521d6fbb615e2b9a749c1c3c65893cdb
Rohan Awhad <rohanavad007@gmail.com>
2022-06-01 16:58:00 +0530

    updated method to detect abbreviations

--- main.py
+++ main.py
@@ -1,69 +1,17 @@
-import torch
-
-from nltk.tokenize import sent_tokenize
-from transformers import AutoTokenizer, AutoModelForTokenClassification
-
-from src import predict
-
-MAX_LENGTH = 35
-OVERLAP = 8
-
-LABEL_TO_NUM_DICT = {
-    "O": 0,
-    "B-long": 1,
-    "B-short": 2,
-    "I-long": 3,
-    "I-short": 4,
-}
-NUM_TO_LABEL_DICT = dict(((n, s) for s, n in LABEL_TO_NUM_DICT.items()))
-
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-TOKENIZER = AutoTokenizer.from_pretrained(
-    "allenai/scibert_scivocab_cased", do_lower_case=False
-)
-saved_model = AutoModelForTokenClassification.from_pretrained(
-    "allenai/scibert_scivocab_cased", num_labels=len(LABEL_TO_NUM_DICT)
-)
-saved_model.load_state_dict(
-    torch.load(
-        "models/abbreviation_detector_v2.pt",
-        map_location=torch.device(DEVICE),
-    )
+import logging
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
 )
 
+import spacy
 
-def run(text):
-    batch = [sent for sent in sent_tokenize(text)]
-
-    tokenized_inputs = TOKENIZER(batch, padding=True, return_tensors="pt")
-    with torch.no_grad():
-        outputs = saved_model(**tokenized_inputs)
-
-    logits = outputs.logits
-    output = logits.argmax(-1).numpy()
-    output = [[NUM_TO_LABEL_DICT[y] for y in x] for x in output]
-
-    compiled_output = predict.compile_model_output(
-        TOKENIZER,
-        tokenized_inputs["input_ids"],
-        output,
-        OVERLAP,
-    )
-    print(compiled_output)
-    predict.removing_single_letter_abbreviations(compiled_output)
-    predict.remove_mislabeled_O_in_btwn_B_I(compiled_output)
-    long_short_pairs = predict.get_long_short_pairs(compiled_output)
-    short_forms = predict.get_short_forms(compiled_output)
-
-    return long_short_pairs, short_forms
+from src.detector import AbbreviationDetector
 
 
-if __name__ == "__main__":
-    text = "The 2002–3 pandemic caused by severe acute respiratory syndrome coronavirus (SARS-CoV) was one of the most significant public health events in recent history(1). An ongoing outbreak of Middle East respiratory syndrome coronavirus (MERS-CoV)(2) suggests that this group of viruses remains a major threat and that their distribution is wider than previously recognized. Although bats have been suggested as the natural reservoirs of both viruses(3–5), attempts to isolate the progenitor virus of SARS-CoV from bats have been unsuccessful. Diverse SARS-like coronaviruses (SL-CoVs) have now been reported from bats in China, Europe and Africa(5–8), but none are considered a direct progenitor of SARS-CoV because of their phylogenetic disparity from this virus and the inability of their spike proteins (S) to use the SARS-CoV cellular receptor molecule, the human angiotensin converting enzyme II (ACE2)(9,10). Here, we report whole genome sequences of two novel bat CoVs from Chinese horseshoe bats (Family: Rhinolophidae) in Yunnan, China; RsSHC014 and Rs3367. These viruses are far more closely related to SARS-CoV than any previously identified bat CoVs, particularly in the receptor binding domain (RDB) of the S protein. Most importantly, we report the first recorded isolation of a live SL-CoV (bat SL-CoV-WIV1) from bat fecal samples in Vero E6 cells, which has typical coronavirus morphology, 99.9% sequence identity to Rs3367 and uses the ACE2s from human, civet and Chinese horseshoe bat for cell entry. Preliminary in vitro testing indicates that WIV1 also has a broad species tropism. Our results provide the strongest evidence to date that Chinese horseshoe bats are natural reservoirs of SARS-CoV, and that intermediate hosts may not be necessary for direct human infection by some bat SL-CoVs. They also highlight the importance of pathogen discovery programs targeting high-risk wildlife groups in emerging disease hotspots as a strategy for pandemic preparedness."
-    pairs, acronyms = run(text)
-    print()
-    print("Pairs")
-    print(pairs)
-    print()
-    print("Acronyms")
-    print(acronyms)
+text = "The 2002–3 pandemic caused by severe acute respiratory syndrome coronavirus (SARS-CoV) was one of the most significant public health events in recent history(1). An ongoing outbreak of Middle East respiratory syndrome coronavirus (MERS-CoV)(2) suggests that this group of viruses remains a major threat and that their distribution is wider than previously recognized. Although bats have been suggested as the natural reservoirs of both viruses(3–5), attempts to isolate the progenitor virus of SARS-CoV from bats have been unsuccessful. Diverse SARS-like coronaviruses (SL-CoVs) have now been reported from bats in China, Europe and Africa(5–8), but none are considered a direct progenitor of SARS-CoV because of their phylogenetic disparity from this virus and the inability of their spike proteins (S) to use the SARS-CoV cellular receptor molecule, the human angiotensin converting enzyme II (ACE2)(9,10). Here, we report whole genome sequences of two novel bat CoVs from Chinese horseshoe bats (Family: Rhinolophidae) in Yunnan, China; RsSHC014 and Rs3367. These viruses are far more closely related to SARS-CoV than any previously identified bat CoVs, particularly in the receptor binding domain (RDB) of the S protein. Most importantly, we report the first recorded isolation of a live SL-CoV (bat SL-CoV-WIV1) from bat fecal samples in Vero E6 cells, which has typical coronavirus morphology, 99.9% sequence identity to Rs3367 and uses the ACE2s from human, civet and Chinese horseshoe bat for cell entry. Preliminary in vitro testing indicates that WIV1 also has a broad species tropism. Our results provide the strongest evidence to date that Chinese horseshoe bats are natural reservoirs of SARS-CoV, and that intermediate hosts may not be necessary for direct human infection by some bat SL-CoVs. They also highlight the importance of pathogen discovery programs targeting high-risk wildlife groups in emerging disease hotspots as a strategy for pandemic preparedness."
+nlp = spacy.load('en_core_sci_sm')
+nlp.add_pipe('abbreviation_detector', config={'config_path': 'models/config.json', 'tokenizer_path': 'allenai/scibert_scivocab_cased', 'model_path': 'models/abbreviation_detector_v2.pt'})
+doc = nlp(text)
+print(doc._.abbreviation_pairs)
---
+++ src/detector.py
@@ -0,0 +1,113 @@
+import logging
+import torch
+
+from spacy.language import Language
+from spacy.tokens import Doc, Span
+from transformers import pipeline, AutoConfig, AutoTokenizer, AutoModelForTokenClassification
+
+logger = logging.getLogger(__name__)
+
+
+
+@Language.factory("abbreviation_detector")
+class AbbreviationDetector:
+    pipe = None
+
+    def __init__(
+        self,
+        nlp: Language,
+        config_path: str,
+        tokenizer_path: str,
+        model_path:str,
+        name: str = 'abbreviation_detector',
+        make_serializable: bool = False,
+    ) -> None:
+
+        Doc.set_extension("abbreviations", default=[], force=True)
+        Doc.set_extension("abbreviation_pairs", default=[], force=True)
+        Doc.set_extension("long2short", default={}, force=True)
+        Doc.set_extension("short2long", default={}, force=True)
+        Span.set_extension("long_form", default=None, force=True)
+        Span.set_extension("is_short_form", default=False, force=True)
+
+        self.make_serializable = make_serializable
+        self.init_pipeline(config_path, tokenizer_path, model_path)
+
+    def init_pipeline(self, config_path, tokenizer_path, model_path):
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        config = AutoConfig.from_pretrained(config_path)
+        tokenizer = AutoTokenizer.from_pretrained(
+            tokenizer_path, do_lower_case=False
+        )
+        model = AutoModelForTokenClassification.from_config(config)
+        model.load_state_dict(
+            torch.load(
+                model_path,
+                map_location=torch.device(device),
+            )
+        )
+        self.pipe = pipeline('token-classification', model=model, tokenizer=tokenizer, use_fast=True, aggregation_strategy='first')
+        logging.info('transformers pipeline ready')
+
+    def __call__(self, doc: Doc) -> Doc:
+        output = self.pipe(doc.text)
+        logger.debug(output)
+
+        # get long-short pairs
+        all_long_short_pairs = []
+        prev_detection = output[0]
+        for pred in output[1:]:
+            if prev_detection['entity_group'] == 'Long' and pred['entity_group'] == 'Short':
+                lf = doc.char_span(prev_detection['start'], prev_detection['end'], alignment_mode='expand')
+                sf = doc.char_span(pred['start'], pred['end'], alignment_mode='expand')
+                all_long_short_pairs.append((lf, sf))
+
+            prev_detection = pred
+
+        doc._.abbreviation_pairs = [self._serialize_pairs(pair) for pair in all_long_short_pairs]
+        doc._.long2short = dict((lf.text, sf.text) for lf, sf in all_long_short_pairs)
+        doc._.short2long = dict((sf.text, lf.text) for lf, sf in all_long_short_pairs)
+
+        logger.info(f'got {len(doc._.abbreviation_pairs)} abbreviation pairs')
+
+        # check if multiple long forms exist for the same short form
+        all_short = set()
+        for _, sf in all_long_short_pairs:
+            if sf.text in all_short:
+                raise "Multiple short-forms present !"
+
+            all_short.add(sf)
+
+        # tag short forms
+        short_form_list = []
+        for pred in output:
+            if pred['entity_group'] == 'Long':
+                continue
+
+            short_form = doc.char_span(pred['start'], pred['end'], alignment_mode='expand')
+            short_form._.is_short_form = True
+            short_form._.long_form = doc._.short2long.get(short_form.text, None)
+            short_form_list.append(short_form)
+
+        doc._.abbreviations = [self._serialize_short_form(short_form) for short_form in short_form_list]
+        logger.info(f'got {len(doc._.abbreviations)} short forms')
+
+        return doc
+
+    def _serialize_short_form(self, short_form):
+        return {
+            'short_form': short_form.text,
+            'start': short_form.start,
+            'end': short_form.end
+        }
+
+    def _serialize_pairs(self, pair):
+        long_form, short_form = pair
+        return {
+            "short_form": short_form.text,
+            "short_form_start": short_form.start,
+            "short_form_end": short_form.end,
+            "long_form": long_form.text,
+            "long_form_start": long_form.start,
+            "long_form_end": long_form.end,
+        }
